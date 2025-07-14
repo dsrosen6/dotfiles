27478d96bd045512c3f69194a998ebb2c486c71f
Danny Rosenthal <dsrosen6@gmail.com>
2025-07-14 18:04:14 -0500

    Add .config/hypr/hyprpaper.conf Add .config/hypr/scripts/wofi_toggle.sh

---
+++ private_dot_config/hypr/scripts/executable_wofi_toggle.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# if wofi is running, exit it, else show it
+# this provides a macOS spotlight-esque "toggle" when run with a keybind
+pgrep wofi >/dev/null 2>&1 && killall wofi || wofi --show drun
