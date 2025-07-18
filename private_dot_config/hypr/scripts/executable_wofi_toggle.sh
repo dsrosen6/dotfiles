1236e6c8c1a443f58ca471b2ae56d3640e8a1537
Danny Rosenthal <dsrosen6@gmail.com>
2025-07-18 12:45:15 -0500

    Update .config/hypr/hypridle.conf Update .config/hypr/hyprland.conf Update .config/hypr/hyprpaper.conf Update .config/hypr/scripts/wofi_toggle.sh

--- private_dot_config/hypr/scripts/executable_wofi_toggle.sh
+++ private_dot_config/hypr/scripts/executable_wofi_toggle.sh
@@ -2,4 +2,5 @@
 
 # if wofi is running, exit it, else show it
 # this provides a macOS spotlight-esque "toggle" when run with a keybind
-pgrep wofi >/dev/null 2>&1 && killall wofi || wofi --show drun
+#pgrep wofi >/dev/null 2>&1 && killall wofi || uwsm app -- "$(wofi --show drun --define=drun-print_desktop_file=true | sed -E "s/(\.desktop) /\1:/")" 
+uwsm app -- "$(wofi --show drun --define=drun-print_desktop_file=true | sed -E "s/(\.desktop) /\1:/")"
