11481620b947b1a8cf1e3f3f6f28b7502dc8e179
Danny Rosenthal <dsrosen6@gmail.com>
2025-07-22 18:37:07 -0500

    Remove .config/hypr/scripts/wofi_toggle.sh

--- private_dot_config/hypr/scripts/executable_wofi_toggle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# if wofi is running, exit it, else show it
-# this provides a macOS spotlight-esque "toggle" when run with a keybind
-#pgrep wofi >/dev/null 2>&1 && killall wofi || uwsm app -- "$(wofi --show drun --define=drun-print_desktop_file=true | sed -E "s/(\.desktop) /\1:/")" 
-uwsm app -- "$(wofi --show drun --define=drun-print_desktop_file=true | sed -E "s/(\.desktop) /\1:/")"
