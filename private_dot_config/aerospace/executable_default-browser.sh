8b5d84c4f639269dc94dce0517c29ff29505e176
tctgdanny <154454046+tctgdanny@users.noreply.github.com>
2025-08-03 17:09:34 -0500

    Update .config/aerospace/default-browser.sh

--- private_dot_config/aerospace/executable_default-browser.sh
+++ private_dot_config/aerospace/executable_default-browser.sh
@@ -2,7 +2,7 @@
 
 # The only MBP I use is for work, so only return Chrome if I'm on one.
 if system_profiler SPHardwareDataType | grep -q "MacBook Pro"; then
-  echo "/Applications/Google Chrome.app"
+    echo "/Applications/Google Chrome.app"
 else
-  echo "/Applications/Brave Browser.app"
+    echo "/Applications/Firefox.app/"
 fi
