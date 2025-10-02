a2c8c8a4776f7294035f30d6d29bc38b04cbae05
Danny Rosenthal <dsrosen6@gmail.com>
2025-10-02 10:56:35 -0500

    switch back to gnome/paperwm bc window managers were ruining my life

--- private_dot_config/sway/executable_clamshell.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if grep -q open /proc/acpi/button/lid/LID/state; then
-    echo "lid is open, enabling eDP-1"
-    swaymsg output eDP-1 enable
-else
-    echo "lid is closed, disabling eDP-1"
-    swaymsg output eDP-1 disable
-fi
--- private_dot_config/waybar/scripts/executable_toggle.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if ! pgrep "waybar"; then
-    systemctl --user start waybar.service
-else
-    systemctl --user stop waybar.service
-fi
