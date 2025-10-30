3f8ca13eac0355150f9ea1e2b8d2dd3da06faf67
Danny Rosenthal <dsrosen6@gmail.com>
2025-10-29 21:23:51 -0500

    thi,nkpa d

---
+++ private_dot_config/walker/menus/executable_powermenu.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+choices="Lock\nSleep\nLog Out\nReboot\nShut Down"
+option=$(echo -e "$choices" | walker --dmenu --hideqa --maxheight 200)
+
+case "$option" in
+"Lock") loginctl lock-session ;;
+"Sleep") systemctl suspend ;;
+"Log Out") hyprctl dispatch exit ;;
+"Reboot") reboot ;;
+"Shut Down") shutdown now ;;
+*) exit 1 ;;
+esac
