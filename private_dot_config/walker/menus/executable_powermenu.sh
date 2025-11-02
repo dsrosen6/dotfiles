46577f05774c37ac7a3b453edb649f3ec9c0576a
Danny Rosenthal <dsrosen6@gmail.com>
2025-11-02 10:01:44 -0600

    re-add lock to power menu

--- private_dot_config/walker/menus/executable_powermenu.sh
+++ private_dot_config/walker/menus/executable_powermenu.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-choices="Sleep\nLog Out\nReboot\nShut Down"
+choices="Lock\nSleep\nLog Out\nReboot\nShut Down"
 option=$(echo -e "$choices" | walker --dmenu --hideqa --maxheight 200)
 
 case "$option" in
+"Lock") swaylock ;;
 "Sleep") systemctl suspend ;;
 "Log Out") hyprctl dispatch exit ;;
 "Reboot") reboot ;;
