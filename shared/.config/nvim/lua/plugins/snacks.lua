bfb0b3ddd88af9559f3bc0f104032e6c908d9f4b
Danny Rosenthal <dsrosen6@gmail.com>
2026-06-14 16:35:28 -0500

    some snacks changes (me when I decide to buy cheez its instead of doritos)

--- shared/.config/nvim/lua/plugins/snacks.lua
+++ shared/.config/nvim/lua/plugins/snacks.lua
@@ -2,7 +2,37 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
-      dashboard = { enabled = false },
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+      },
+    },
+    keys = {
+      -- Switching the lazyvim default because I prefer leader-e for cwd instead of git root
+      {
+        "<leader>fE",
+        function()
+          Snacks.explorer({ cwd = LazyVim.root() })
+        end,
+        desc = "Explorer Snacks (root dir)",
+      },
+      {
+        "<leader>fe",
+        function()
+          Snacks.explorer()
+        end,
+        desc = "Explorer Snacks (cwd)",
+      },
+      { "<leader>E", "<leader>fE", desc = "Explorer Snacks (root dir)", remap = true },
+      { "<leader>e", "<leader>fe", desc = "Explorer Snacks (cwd)", remap = true },
     },
   },
 }
